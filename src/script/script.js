34c925c62c217b2c7a5220a28fe6b61f43ddecba
Krzysztof-K94 <krzysztofkukielka94@gmail.com>
2022-05-06 16:57:21 +0200

    Navigation

---
+++ src/script/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const btn = document.querySelector('.navigation__button');
+const nav = document.querySelector('.navigation');
+const body = document.querySelector('body');
+
+console.log(btn);
+console.log(nav)
+
+
+console.log('body')
