2494f1feaecf6d8a8c339c2a4fb0f55a0e2cf6ec
Krzysztof-K94 <krzysztofkukielka94@gmail.com>
2022-05-09 13:40:17 +0200

    responsive layout

--- src/script/script.js
+++ src/script/script.js
@@ -2,10 +2,21 @@
 
 const btn = document.querySelector('.navigation__button');
 const nav = document.querySelector('.navigation');
-const body = document.querySelector('body');
+const popup = document.querySelector('.popup');
+const popupLinks = document.querySelectorAll('.btn[href="#popup"]');
+const closePopup = document.querySelector('.popup__close');
 
-console.log(btn);
-console.log(nav)
 
+btn.addEventListener('click', function() {
+    nav.classList.toggle('open');
+});
 
-console.log('body')
+popupLinks.forEach(link => {
+    link.addEventListener('click', function(){
+        popup.classList.add('open');
+    })
+});
+
+closePopup.addEventListener('click', function() {
+    popup.classList.remove('open');
+})
